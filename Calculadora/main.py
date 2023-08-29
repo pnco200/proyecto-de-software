a4c3006119700aadea636514389ff2b5f297b80f
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-08-28 22:02:31 -0300

    Agregada operacion suma y resta al main

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,4 +1,12 @@
+from operaciones import suma, multiplicacion
+
+
 x = int(input("Inserte el primer numero: "))
 
 y = int(input("Inserte el segundo numero: "))
 
+
+print("La suma es: ", suma(x, y))
+#print("La resta es: ", resta(x, y))
+print("La multiplicacion es: ", multiplicacion(x, y))
+#print("La division es: ", division(x, y))
