5d99941abb3788ad442eee4598af5f3fd7078c4f
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-08-29 23:27:20 -0300

    Agregada funcionalidad de division

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,4 +1,4 @@
-from operaciones import suma, multiplicacion, resta
+from operaciones import suma, multiplicacion, resta, division
 
 
 x = int(input("Inserte el primer numero: "))
@@ -9,4 +9,7 @@ y = int(input("Inserte el segundo numero: "))
 print("La suma es: ", suma(x, y))
 print("La resta es: ", resta(x, y))
 print("La multiplicacion es: ", multiplicacion(x, y))
-#print("La division es: ", division(x, y))
+try:
+    print("La division es: ", division(x, y))
+except ZeroDivisionError:
+    print("No se puede dividir entre 0")
