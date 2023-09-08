bd28092c23b178fd80201fa0a8c344d625809023
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-09-08 12:35:04 -0300

    Aplicados los templates al controlador del home

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     @app.get("/")
     def home():
-        return "Hola mundo!"
-    
+        return render_template("home.html")
+
     return app
