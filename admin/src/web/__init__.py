3a8d72763f3db2df204afe07dfd13c4b2e92f264
GastonAmantea <gaston14401@gmail.com>
2023-09-23 16:22:41 -0300

    añadido modulo de envio de mails

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -4,19 +4,25 @@ from src.core import database
 from src.web.config import config
 from src.web import error
 from src.web.controllers.test import test_bp
+from src.core.email import email_utils
 #from src.web.controllers.issues import issues
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
-    print(config[env])
     app.config.from_object(config[env])
 
     database.init_app(app)
+    email_utils.init_app(app)
 
     app.register_blueprint(test_bp)
 
     @app.get("/")
     def home():
+        return render_template("home.html")
+    
+    @app.get("/sendmailtest")
+    def mail_test():
+        email_utils.send_email("Prueba",["nicolaspanico2002@gmail.com"],"Esto es una prueba. </br> Funciono?")
         return render_template("home.html")
 
     app.register_error_handler(404,error.not_found_error404)
