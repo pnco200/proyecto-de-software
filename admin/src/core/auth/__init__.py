e4e812aa903ef661507d8d285778e0bc4eb56965
GastonAmantea <gaston14401@gmail.com>
2023-10-06 20:25:07 -0300

    Terminado proceso de registro y añadido campos extra en el formulario

--- admin/src/core/auth/__init__.py
+++ admin/src/core/auth/__init__.py
@@ -1,6 +1,7 @@
 from src.core.database import db
 from src.core.auth.user import User
 from src.core.bcrypt import bcrypt
+from sqlalchemy import or_
 def list_users():
     return User.query.all()
     
@@ -13,8 +14,10 @@ def create_user(**kwargs):
     return user
 
 def find_user_by_email(email):
-    print(User.query.filter_by(email=email).first())
     return User.query.filter_by(email=email).first()
+
+def find_user_by_email_or_username(email, username):
+    return User.query.filter(or_(email == email,username == username)).first()
 
 def check_user(email,password):
     user = find_user_by_email(email)
@@ -22,3 +25,13 @@ def check_user(email,password):
         return user
     else:
         return None
+    
+def confirm_email(token):
+    user = User.query.filter_by(confirm_token=token).first()
+    if user: 
+        user.is_confirmed = True
+        db.session.commit()
+        return user  
+    else:
+        return None
+        
