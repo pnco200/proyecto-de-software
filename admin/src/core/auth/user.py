06abe6d48738a7502612f2e86e5075ed461fefc5
GastonAmantea <gaston14401@gmail.com>
2023-10-10 18:00:09 -0300

    Añadido modulo de configuracion de sitio

--- admin/src/core/auth/user.py
+++ admin/src/core/auth/user.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__= "users"
     id = db.Column(db.Integer, primary_key=True, unique=True)
     email = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50), unique=True)
-    lastname = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=False)
+    lastname = db.Column(db.String(50), unique=False)
     username = db.Column(db.String(50), unique=True)
     is_confirmed = db.Column(db.Boolean, unique=False, default=False)
     is_active = db.Column(db.Boolean, unique=False, default=True)
