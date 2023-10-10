f2b07310886aebd8ff0abc2d4d560d89fc0bad18
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-10-10 16:34:09 -0300

    Deleted Test

--- admin/src/core/test/test.py
+++
@@ -1,8 +0,0 @@
-from src.core.database import db
-
-
-class Test(db.Model):
-    """"Modelo Test en la base de datos"""
-    __tablename__ = "test"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(255), nullable=False)
