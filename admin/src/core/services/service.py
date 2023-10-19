904c467dc3364d5f18d57a0eac23974c2cea4603
ignacio mariano <ignaciomarianomosca@gmail.com>
2023-10-19 11:23:56 -0300

    tablas relacionadas a solicitudes de servicios

--- admin/src/core/services/service.py
+++ admin/src/core/services/service.py
@@ -25,3 +25,40 @@ class Service(db.Model):
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
     inserted_at= db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class service_state(db.Model):
+    __tablename__ = "service_state"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50), unique=False)
+    state_message = db.Column(db.String(255), unique=False)
+    updated_at=db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    inserted_at= db.Column(db.DateTime, default=datetime.utcnow)
+
+class Service_Request(db.Model):
+    __tablename__ = "service_request"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50), unique=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    state_id = db.Column(db.Integer, db.ForeignKey('service_state.id'))
+    archive = db.Column(db.LargeBinary)    
+    observations = db.Column(db.String(255), unique=False)
+    updated_at=db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    inserted_at= db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Service_Request_Message(db.Model):
+    __tablename__ = "service_request_message"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50), unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    msg_content = db.Column(db.String(255), unique=False )
+    updated_at=db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    inserted_at= db.Column(db.DateTime, default=datetime.utcnow)
