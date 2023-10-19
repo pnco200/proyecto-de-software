568796962058a8c14c5d738a4c8ebd9d5644b1c8
GastonAmantea <gaston14401@gmail.com>
2023-10-19 15:04:20 -0300

    auth endpoint

--- admin/src/core/auth/__init__.py
+++ admin/src/core/auth/__init__.py
@@ -42,7 +42,7 @@ def find_user_by_username(username):
 
 def check_user(email,password):
     user = find_user_by_email(email)
-    if user and bcrypt.check_password_hash(user.password, password.encode("utf-8")):
+    if user and bcrypt.check_password_hash(user.password, str(password).encode("utf-8")):
         return user
     else:
         return None
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -11,6 +11,8 @@ from src.web.controllers.institution import institution_bp
 from src.web.controllers.permissions import permissions_bp
 from src.web.controllers.services import service_bp
 from src.web.api.institutions import api_institution_bp
+from src.web.api.auth import api_auth_bp
+
 from src.web.helpers import auth
 from src.web.helpers import utils
 from src.web.helpers import permissions
@@ -37,7 +39,9 @@ def create_app(env="development", static_folder="../../static"):
     app.register_blueprint(institution_bp)
     app.register_blueprint(permissions_bp)
     app.register_blueprint(service_bp)
+    #API BLUEPRINTS
     app.register_blueprint(api_institution_bp)
+    app.register_blueprint(api_auth_bp)
 
     def get_user_institutions():
         return institutions.get_user_institutions(session.get("user"),utils.current_selected_institution())
