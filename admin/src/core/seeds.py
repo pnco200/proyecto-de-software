7bfdb80efbb65e9c75dfb1d4ee0b436328cea31d
ignacio mariano <ignaciomarianomosca@gmail.com>
2023-10-20 13:07:59 -0300

    seeds para  solicitudes, cree servicios y usuarios para cada solicitud

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -1,4 +1,4 @@
-from src.core import institutions, configuration, auth, rol_permission, services
+from src.core import institutions, configuration, auth, rol_permission, services, service_requests
 
 def run():
     def create_institutions():
@@ -21,7 +21,7 @@ def run():
             web="https://portal.if.usp.br/",
             keywords="Física",
             attention_time="Segunda à Sexta das 8h às 17h",
-            contact="11 3091-6149",
+            contact="11 309-6149",
             is_active=True
         )
         institutions.create_institution(
@@ -32,7 +32,7 @@ def run():
             web="https://www.iq.usp.br/",
             keywords="Química",
             attention_time="Segunda à Sexta das 8h às 17h",
-            contact="11 3091-6149",
+            contact="11 091-6149",
             is_active=True
         )
         institutions.create_institution(
@@ -43,7 +43,7 @@ def run():
             web="https://ib.usp.br/",
             keywords="Biologia",
             attention_time="Segunda à Sexta das 8h às 17h",
-            contact="11 3091-6149",
+            contact="11 91-6149",
             is_active=True
         )
         institutions.create_institution(
@@ -54,7 +54,7 @@ def run():
             web="https://www.ip.usp.br/",
             keywords="Psicologia",
             attention_time="Segunda à Sexta das 8h às 17h",
-            contact="11 3091-6149",
+            contact="11 3091-49",
             is_active=True
         )
         institutions.create_institution(
@@ -65,7 +65,7 @@ def run():
             web="https://icb.usp.br/",
             keywords="Biologia, Medicina",
             attention_time="Segunda à Sexta das 8h às 17h",
-            contact="11 3091-6149",
+            contact="11 3091-9",
             is_active=True
         )
     def create_users():
@@ -561,6 +561,129 @@ def run():
             role_id = "SuperAdmin"
         )
         
+    def   create_solcitudes_Example_1():
+        user = auth.create_user(
+            email="mail30@gmail.com",
+            name="Jay",
+            lastname="Dem",
+            username="jaye",
+            password="1453",
+            is_confirmed=True,
+            is_active=True
+        )
+        service =  services.create_service(
+            name="Química1",
+            type="ANALISIS",
+            centers="IQ",
+            description="Química uno",
+            key_words=["Quimica1"],
+            enabled=True,
+            institution_id=2
+        )
+        state = service_requests.create_state_request(
+            name="incial",
+            state_message = "estado inicial"
+        )
+        solicitude = service_requests.create_service_request(
+            user_id = user.id,
+            service_id = service.id,
+            state_id = state.id,
+            observations ="quiero que se le haga a la pintura un nuevo tratamiento en la capa de pintura exterior",
+            archive = None
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "Quisiera aclarar que quiero que se pueda aplicar a cuantas campas sea posible"            
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "En lo posible quisiera conocer un costo estimado"            
+        )
+    def   create_solcitudes_Example_2():
+        user = auth.create_user(
+            email="mail10@gmail.com",
+            name="Juanito",
+            lastname="guay",
+            username="juan2",
+            password="2009",
+            is_confirmed=True,
+            is_active=True
+        )
+        service =  services.create_service(
+            name="Química3",
+            type="ANALISIS",
+            centers="IQ",
+            description="Química tres",
+            key_words=["Quimica3"],
+            enabled=True,
+            institution_id=2
+        )
+        state = service_requests.create_state_request(
+            name="incial",
+            state_message = "estado inicial"
+        )
+        solicitude = service_requests.create_service_request(
+            user_id = user.id,
+            service_id = service.id,
+            state_id = state.id,
+            observations ="aplicar un tratamiento para conocer la edad de la pintura",
+            archive = None
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "conocer que tipo de pintura se utilizo"            
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "En lo posible quisiera conocer un costo estimado"            
+        )
+    def   create_solcitudes_Example_3():
+        user = auth.create_user(
+            email="mail20@gmail.com",
+            name="roman",
+            lastname="tamar",
+            username="je",
+            password="13456",
+            is_confirmed=True,
+            is_active=True
+        )
+        service =  services.create_service(
+            name="Psicologia2",
+            type="ANALISIS",
+            centers="IQ",
+            description="psicologia dos",
+            key_words=["Psicologia2"],
+            enabled=True,
+            institution_id=5
+        )
+        state = service_requests.create_state_request(
+            name="incial",
+            state_message = "estado inicial"
+        )
+        solicitude = service_requests.create_service_request(
+            user_id = user.id,
+            service_id = service.id,
+            state_id = state.id,
+            observations ="conocer el significado real de la pintura",
+            archive = None
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "tener conocimiento sobre la persona que hizo la pintura, que aspecto se pueden conocer"            
+        )
+        message = service_requests.create_message_request(
+            user_id = solicitude.user_id,
+            service_request_id = service_requests.id,
+            msg_content = "En lo posible quisiera conocer un costo estimado"            
+        )
+        
+        
+        
     create_configurations()
     create_institutions()
     create_users()
@@ -568,5 +691,8 @@ def run():
     create_roles()
     create_permissions()
     create_rol_has_these_permission()
+    create_solcitudes_Example_1()##De instituto de fisica id=2
+    create_solcitudes_Example_2()
+    create_solcitudes_Example_3()##De instuto de psicologia id=5
 
 
