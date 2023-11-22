a56cbb49fdc52d4ff578213266a48cd0e4ee1071
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-11-22 14:28:35 -0300

    Config endpoint added

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -10,6 +10,7 @@ from src.web.controllers.users import user_bp
 from src.web.controllers.configuration import config_bp
 from src.web.controllers.institution import institution_bp
 from src.web.controllers.permissions import permissions_bp
+from src.web.api.configuration import api_configuration_bp
 from src.web.controllers.services import service_bp
 from src.web.api.institutions import api_institution_bp
 from src.web.api.auth import api_auth_bp
@@ -49,12 +50,13 @@ def create_app(env="development", static_folder="../../static"):
     app.register_blueprint(permissions_bp)
     app.register_blueprint(service_bp)
     app.register_blueprint(srequest_bp)
-    app.register_blueprint(api_service_bp)
 
     #API BLUEPRINTS
     app.register_blueprint(api_institution_bp)
     app.register_blueprint(api_user_bp)
     app.register_blueprint(api_auth_bp)
+    app.register_blueprint(api_service_bp)
+    app.register_blueprint(api_configuration_bp)
 
     def get_user_institutions():
         return institutions.get_user_institutions(session.get("user"), utils.current_selected_institution())
