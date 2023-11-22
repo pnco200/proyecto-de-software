1427322225dbca808424b811d9e75c36022c6eb4
PANICO NICOLAS <nicolaspanico2002@gmail.com>
2023-11-22 15:01:40 -0300

    Comenzado graficos

--- portal/src/components/BarChart.vue
+++ portal/src/components/BarChart.vue
@@ -3,53 +3,58 @@
 </template>
 
 <script lang="js">
-import { defineComponent, onMounted, ref } from 'vue';
-import { DoughnutChart } from 'vue-chart-3';
-import { Chart, registerables } from 'chart.js';
+import { defineComponent, onMounted, ref } from 'vue'
+import { DoughnutChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+import axios from 'axios'
 
-
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 const fetchData = async () => {
     try {
-        const response = await fetch('http://localhost:5000/api/stats/requests-institutions');
-        const data = await response.json();
-		console.log(data);
-        testData.value = processResponse(data.labels, data.values);
+        const response = await axios.get('http://localhost:5000/api/stats/requests-institutions')
+        const data = response.data
+        console.log(data)
+        return processResponse(data)
     } catch (error) {
-        console.error('Error fetching or processing data:', error);
+        console.error('Error fetching or processing data:', error)
+        // Handle error or return a default value
+        return { labels: [], datasets: [] }
     }
-};
+}
 
-const processResponse = (labels, values) => {
+const processResponse = (data) => {
+    const labels = data.map((item) => item.Institucion)
+    const values = data.map((item) => item.Solicitudes)
+
     return {
         labels,
         datasets: [
             {
                 data: values,
-                backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
-            },
-        ],
-    };
-};
+                backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED']
+            }
+        ]
+    }
+}
 
 export default defineComponent({
     name: 'testComponent',
     components: { DoughnutChart },
 
     setup() {
-        const testData = ref(null);
+        const testData = ref(null)
 
         onMounted(async () => {
             try {
-                const response = await fetchData();
-                testData.value = processResponse(response);
+                const processedData = await fetchData()
+                testData.value = processedData
             } catch (error) {
-                console.error('Error fetching or processing data:', error);
+                console.error('Error fetching or processing data:', error)
             }
-        });
+        })
 
-        return { testData };
-    },
-});
+        return { testData }
+    }
+})
 </script>
