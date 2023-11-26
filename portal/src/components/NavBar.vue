29b8a6201e62ab128cf872d37c339a165a8a4a9c
Ramiro Gutierrez Paulos <ramagp00@gmail.com>
2023-11-26 11:17:52 -0300

    NavBar responsive

--- portal/src/components/NavBar.vue
+++ portal/src/components/NavBar.vue
@@ -3,22 +3,21 @@
         <div class="container-fluid">
 
             <router-link to="/" class="navbar-brand">
-                <img src="../assets/cidepint.png" alt="Logo">
+                <img src="../assets/cidepint.png" alt="Logo" class="img-fluid">
             </router-link>
 
-            <div v-if="!logged">
-                <router-link to="/login">
-                Iniciar Sesion
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                </svg>
+            <div v-if="!logged" class="d-flex flex-row">
+                <router-link to="/login" class="btn btn-primary ml-auto">
+                    Iniciar Sesion
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
                 </router-link>
             </div>
-            <div v-else>
-                <span @click="see_requests">Ver Solicitudes  </span>
-                
-                <span @click="close_session">Cerrar Sesion</span>
+            <div v-else class="d-flex flex-row">
+                <span @click="see_requests" class="btn btn-primary ml-auto">Ver Solicitudes</span>
+                <span @click="close_session" class="btn btn-danger ml-2">Cerrar Sesion</span>
             </div>
         </div>
     </nav>
@@ -26,31 +25,45 @@
 
 <style scoped>
 .navbar-brand img {
-    width: 250px;
-    height: 100px;
-    margin: 0;
-    padding: 0;
+    width: 100%;
+    height: auto;
 }
+
 .navbar {
-    padding: 0;
-    margin: 0;
-    height: 100px;
+    padding: 10px;
     background-color: #fff;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 }
 
 svg {
-    width: 30px;
-    height: 50px;
-    margin: 0 20px;
-    padding: 0;
+    width: 1.5em;
+    height: 1.5em;
+    margin-right: 0.5em;
 }
 
-/* Need the input longer */
-form {
-    width: 80%;
+.btn {
+    white-space: nowrap;
+}
+
+@media (max-width: 360px) {
+    .navbar-brand img {
+        width: 80%;
+    }
+}
+
+@media (min-width: 361px) and (max-width: 767px) {
+    .navbar-brand img {
+        width: 150px;
+    }
+}
+
+@media (min-width: 768px) {
+    .navbar-brand img {
+        width: 250px;
+    }
 }
 </style>
+
 <script>
     import Cookies from 'js-cookie';
     export default {
@@ -84,4 +97,3 @@ form {
     },
   };
 </script>
-
